bf9c7f3d79f87602184bb41405ced14f614731c4
Riley Sherlock <riley.sherlock@mts.net>
2022-09-14 17:07:23 -0500

    style proficiency component

--- src/components/Proficiency.js
+++ src/components/Proficiency.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AdditionalInfo from './AdditionalInfo';
 import CycleArrow from './CycleArrow';
+import '../styles/Proficiency.css';
 
 export default function Proficiency(props) {
   function onChange(e) {
@@ -13,8 +14,12 @@ export default function Proficiency(props) {
         <AdditionalInfo></AdditionalInfo>
         <CycleArrow></CycleArrow>
       </div>
-      <label htmlFor="proficiency_value">Proficiency Bonus</label>
+      <label className="proficiency_title" htmlFor="proficiency_value">
+        Proficiency Bonus
+      </label>
       <input
+        id="proficiency_value"
+        className="proficiency_value"
         type="number"
         min="2"
         max="6"
