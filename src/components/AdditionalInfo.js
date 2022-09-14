b78a6cbb498b430c38da859a1954799448167841
Riley Sherlock <riley.sherlock@mts.net>
2022-09-14 13:45:46 -0500

    create statistic component

---
+++ src/components/AdditionalInfo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../styles/SmallButton.css';
+
+export default function AdditionalInfo(props) {
+  return <button className="info_button small_button">?</button>;
+}
---
+++ src/components/CycleArrow.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../styles/SmallButton.css';
+
+export default function CycleArrow(props) {
+  return <button className="cycle_arrow small_button">-></button>;
+}
