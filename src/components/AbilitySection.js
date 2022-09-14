b40cbee4b1c03b3501df476f745c35f2f26a7c26
Riley Sherlock <riley.sherlock@mts.net>
2022-09-14 16:30:56 -0500

    refine styles

--- src/components/AbilitySection.js
+++ src/components/AbilitySection.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Ability from './Ability';
+import CycleArrow from './CycleArrow';
+import AdditionalInfo from './AdditionalInfo';
 import '../styles/AbilitySection.css';
 
 export default function AbilitySection(prop) {
@@ -7,12 +9,22 @@ export default function AbilitySection(prop) {
 
   return (
     <div className="ability_section">
-      <Ability name="strength" proficiency={proficiency}></Ability>
-      <Ability name="dexterity" proficiency={proficiency}></Ability>
-      <Ability name="constitution" proficiency={proficiency}></Ability>
-      <Ability name="intelligence" proficiency={proficiency}></Ability>
-      <Ability name="wisdom" proficiency={proficiency}></Ability>
-      <Ability name="charisma" proficiency={proficiency}></Ability>
+      <div className="ability_section_title">
+        <div className="buttons">
+          <AdditionalInfo></AdditionalInfo>
+          <CycleArrow></CycleArrow>
+        </div>
+        Ability Scores
+      </div>
+
+      <div className="abilities">
+        <Ability name="strength" proficiency={proficiency}></Ability>
+        <Ability name="dexterity" proficiency={proficiency}></Ability>
+        <Ability name="constitution" proficiency={proficiency}></Ability>
+        <Ability name="intelligence" proficiency={proficiency}></Ability>
+        <Ability name="wisdom" proficiency={proficiency}></Ability>
+        <Ability name="charisma" proficiency={proficiency}></Ability>
+      </div>
     </div>
   );
 }
