595bbbc033400c60ea3c5d5c1ff38ccae2550e62
Riley Sherlock <riley.sherlock@mts.net>
2022-09-15 15:57:43 -0500

    style hit points component

--- src/components/HitPoints.js
+++ src/components/HitPoints.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AdditionalInfo from './AdditionalInfo';
 import CycleArrow from './CycleArrow';
-// import '../styles/HitDice.css';
+import '../styles/HitPoints.css';
 
 export default function HitDice(props) {
   let value = props.size + props.constitution_modifier;
