3ecd624fdaccd5d6293dd181d7ee306dd5eef756
Riley Sherlock <riley.sherlock@mts.net>
2022-09-15 13:03:26 -0500

    lift score state up to sheet component

--- src/components/AbilitySection.js
+++ src/components/AbilitySection.js
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Ability from './Ability';
 import CycleArrow from './CycleArrow';
 import AdditionalInfo from './AdditionalInfo';
 import '../styles/AbilitySection.css';
 
 export default function AbilitySection(props) {
-  const [strength, setStrength] = useState(10);
-  const [dexterity, setDexterity] = useState(10);
-  const [constitution, setConstitution] = useState(10);
-  const [intelligence, setIntelligence] = useState(10);
-  const [wisdom, setWisdom] = useState(10);
-  const [charisma, setCharisma] = useState(10);
-
   return (
     <div className="ability_section">
       <div className="ability_section_title">
@@ -26,38 +19,38 @@ export default function AbilitySection(props) {
         <Ability
           name="strength"
           proficiency={props.proficiency}
-          score={strength}
-          onChange={setStrength}
+          score={props.strength}
+          onChange={(v) => props.onScoreChange('strength', v)}
         ></Ability>
         <Ability
           name="dexterity"
           proficiency={props.proficiency}
-          score={dexterity}
-          onChange={setDexterity}
+          score={props.dexterity}
+          onChange={(v) => props.onScoreChange('dexterity', v)}
         ></Ability>
         <Ability
           name="constitution"
           proficiency={props.proficiency}
-          score={constitution}
-          onChange={setConstitution}
+          score={props.constitution}
+          onChange={(v) => props.onScoreChange('constitution', v)}
         ></Ability>
         <Ability
           name="intelligence"
           proficiency={props.proficiency}
-          score={intelligence}
-          onChange={setIntelligence}
+          score={props.intelligence}
+          onChange={(v) => props.onScoreChange('intelligence', v)}
         ></Ability>
         <Ability
           name="wisdom"
           proficiency={props.proficiency}
-          score={wisdom}
-          onChange={setWisdom}
+          score={props.wisdom}
+          onChange={(v) => props.onScoreChange('wisdom', v)}
         ></Ability>
         <Ability
           name="charisma"
           proficiency={props.proficiency}
-          score={charisma}
-          onChange={setCharisma}
+          score={props.charisma}
+          onChange={(v) => props.onScoreChange('charisma', v)}
         ></Ability>
       </div>
     </div>
--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -5,9 +5,28 @@ import SkillSection from './SkillSection';
 
 export default function Sheet(props) {
   const [proficiency, setProficiency] = useState(2);
+  const [strength, setStrength] = useState(10);
+  const [dexterity, setDexterity] = useState(10);
+  const [constitution, setConstitution] = useState(10);
+  const [intelligence, setIntelligence] = useState(10);
+  const [wisdom, setWisdom] = useState(10);
+  const [charisma, setCharisma] = useState(10);
+
+  function modifier(v) {
+    return Math.floor((v - 10) / 2);
+  }
 
   function handleProficiencyChange(p) {
     setProficiency(parseInt(p));
+  }
+
+  function handleScoreChange(name, value) {
+    if (name === 'strength') setStrength(value);
+    else if (name === 'dexterity') setDexterity(value);
+    else if (name === 'constitution') setConstitution(value);
+    else if (name === 'intelligence') setIntelligence(value);
+    else if (name === 'wisdom') setWisdom(value);
+    else if (name === 'charisma') setCharisma(value);
   }
 
   return (
@@ -16,15 +35,24 @@ export default function Sheet(props) {
         value={proficiency}
         onChange={handleProficiencyChange}
       ></Proficiency>
-      <AbilitySection proficiency={proficiency}></AbilitySection>
+      <AbilitySection
+        proficiency={proficiency}
+        strength={strength}
+        dexterity={dexterity}
+        constitution={constitution}
+        intelligence={intelligence}
+        wisdom={wisdom}
+        charisma={charisma}
+        onScoreChange={handleScoreChange}
+      ></AbilitySection>
       <SkillSection
         proficiency={proficiency}
-        strength_modifier={2}
-        dexterity_modifier={2}
-        constitution_modifier={2}
-        intelligence_modifier={2}
-        wisdom_modifier={2}
-        charisma_modifier={2}
+        strength_modifier={modifier(strength)}
+        dexterity_modifier={modifier(dexterity)}
+        constitution_modifier={modifier(constitution)}
+        intelligence_modifier={modifier(intelligence)}
+        wisdom_modifier={modifier(wisdom)}
+        charisma_modifier={modifier(charisma)}
       ></SkillSection>
     </div>
   );
