5014ea9b048e60c904cde0af809136e2db38285f
Riley Sherlock <riley.sherlock@mts.net>
2022-09-15 20:26:04 -0500

    create level component

--- src/components/Character/Level.js
+++ src/components/Character/Level.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import AdditionalInfo from '../AdditionalInfo';
+import CycleArrow from '../CycleArrow';
 
 export default function Level(props) {
-  return <div className="level"></div>;
+  return (
+    <div className="level">
+      <div className="buttons">
+        <AdditionalInfo></AdditionalInfo>
+        <CycleArrow></CycleArrow>
+      </div>
+      <label htmlFor="level" className="level_title">
+        Class
+      </label>
+      <input
+        type="number"
+        name="level"
+        id="level"
+        min="1"
+        max="20"
+        level={props.value}
+        onChange={props.onChange}
+      />
+    </div>
+  );
 }
