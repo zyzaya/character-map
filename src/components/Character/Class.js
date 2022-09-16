028dc6ff82393293e2c7a8e324f9a7ebc604e7a4
Riley Sherlock <riley.sherlock@mts.net>
2022-09-15 20:24:08 -0500

    style class component

--- src/components/Character/Class.js
+++ src/components/Character/Class.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Classes } from '../../info/Classes';
+import AdditionalInfo from '../AdditionalInfo';
+import CycleArrow from '../CycleArrow';
+import '../../styles/Class.css';
 
 export default function Class(props) {
   return (
     <div className="class">
-      <label htmlFor="class"></label>
+      <div className="buttons">
+        <AdditionalInfo></AdditionalInfo>
+        <CycleArrow></CycleArrow>
+      </div>
+      <label htmlFor="class" className="class_title">
+        Class
+      </label>
       <select name="class" id="class">
         {Object.keys(Classes).map((k) => {
           return <option value={k}>{Classes[k].name}</option>;
