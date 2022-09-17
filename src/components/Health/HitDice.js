2e5c861b80dcf53298df68cc1c6b8b0db8231142
Riley Sherlock <riley.sherlock@mts.net>
2022-09-17 16:59:59 -0500

    add relationship visuals to hit dice

--- src/components/Health/HitDice.js
+++ src/components/Health/HitDice.js
@@ -2,10 +2,18 @@ import React from 'react';
 import AdditionalInfo from '../AdditionalInfo';
 import CycleArrow from '../CycleArrow';
 import '../../styles/HitDice.css';
+import useStatistic from '../../hooks/useStatistic';
 
 export default function HitDice(props) {
+  const [visual, setVisual] = useStatistic('hit_dice');
+
+  function toggleVisual(e) {
+    e.preventDefault();
+    setVisual(visual === 'focused' ? 'none' : 'focused');
+  }
+
   return (
-    <div className="hitdice">
+    <div className={`hitdice ${visual}`} onClick={toggleVisual}>
       <div className="hitdice_title">
         <div className="buttons">
           <AdditionalInfo></AdditionalInfo>
