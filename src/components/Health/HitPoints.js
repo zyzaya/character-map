4408c6a2c78ad12cab96448e61db821a112cc73a
Riley Sherlock <riley.sherlock@mts.net>
2022-09-19 14:34:37 -0500

    add visual of health calculation

--- src/components/Health/HitPoints.js
+++ src/components/Health/HitPoints.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Buttons from '../Buttons';
 import '../../styles/HitPoints.css';
 import useStatistic from '../../hooks/useStatistic';
 
 export default function HitDice(props) {
   const [visual, setVisual] = useStatistic('hit_points');
+  const [hitpoints_calc, setHitPointsCalc] = useState('');
+
+  useEffect(() => {
+    // 8 (hit dice size) + c (constitution modifier)
+    // + L (levels after first) * (5 + constitution modifier)
+    let output = `${props.size} (hit dice) + ${props.constitution_modifier} (constitution modifier)`;
+    if (props.level > 1) {
+      output += `\n+ ${props.level - 1} (levels after first) * (${
+        props.size / 2 + 1
+      } + ${props.constitution_modifier} (constitution modifier)`;
+    }
+    setHitPointsCalc(output);
+  }, [props.level, props.size, props.constitution_modifier, hitpoints_calc]);
 
   function toggleVisual(e) {
     e.preventDefault();
@@ -22,6 +35,7 @@ export default function HitDice(props) {
         Hit Point Maximum
       </div>
       <div className="hitpoints_output">{value}</div>
+      <div className="hitpoints_calculation">{hitpoints_calc}</div>
     </div>
   );
 }
