003206efca3054fd7cdabb4e845d973a904c35c8
Riley Sherlock <riley.sherlock@mts.net>
2022-09-19 16:51:50 -0500

    make info popup display off of state

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from 'react';
 import '../styles/Info.css';
 import yaml from 'js-yaml';
 import raw from '../info/Help.yaml';
+import { useSelector } from 'react-redux';
 
 export default function Info(props) {
   let [title, setTitle] = useState('');
   let [body, setBody] = useState('');
   let [data, setData] = useState(undefined);
+  const visible = useSelector((state) => state.info.visible);
+  const key = useSelector((state) => state.info.value);
+
   useEffect(() => {
     fetch(raw)
       .then((response) => response.text())
@@ -22,16 +26,16 @@ export default function Info(props) {
   }, []);
 
   useEffect(() => {
-    if (data === undefined) setTitle('');
-    else setTitle(data.strength_score.title);
-  }, [title, data]);
+    if (data === undefined || data[key] === undefined) setTitle('');
+    else setTitle(data[key].title);
+  }, [title, data, key]);
   useEffect(() => {
-    if (data === undefined) setBody('');
+    if (data === undefined || data[key] === undefined) setBody('');
     else setBody(data.strength_score.info);
-  }, [body, data]);
+  }, [body, data, key]);
 
   return (
-    <div className="info">
+    <div className={`info ${visible ? '' : 'hidden'}`}>
       <div className="info_container">
         <h1 className="info_title">{title}</h1>
         <button className="info_close">Close</button>
