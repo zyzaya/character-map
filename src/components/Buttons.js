77a82e9bd4aa1c8f68e5f4b3e18988fe62163831
Riley Sherlock <riley.sherlock@mts.net>
2022-09-19 13:22:04 -0500

    create buttons component

---
+++ src/components/Buttons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdditionalInfo from './AdditionalInfo';
+
+export default function Buttons(props) {
+  return (
+    <div className="buttons">
+      <AdditionalInfo></AdditionalInfo>
+    </div>
+  );
+}
