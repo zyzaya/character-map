c34428f4ff9dc4b6ec3dad9beb121f56b85cf82c
Riley Sherlock <riley.sherlock@mts.net>
2022-09-26 16:19:05 -0500

    add more information to weapons table

--- src/components/Offense/Weapon.js
+++ src/components/Offense/Weapon.js
@@ -13,19 +13,30 @@ export default function Weapon(props) {
     <div className={`weapon ${visual}`} onClick={toggleVisual}>
       <div className="weapon_title">{props.name}</div>
       <div className="tohit">
-        {'+ '}
-        <span className="attack_modifier">
-          {props.modifier + props.proficiency}
-        </span>
-        {' to hit.'}
+        <div className="tohit_info">
+          {'+ '}
+          <span className="attack_modifier">
+            {props.modifier + props.proficiency}
+          </span>
+          {' to hit.'}
+        </div>
+        <div className="tohit_calculation">
+          {`${props.proficiency} (proficiency) + ${props.modifier} (${props.modifier_name})`}
+        </div>
       </div>
       <div className="damage">
-        <span className="dice_count">{props.dice_count}</span>
-        {'d'}
-        <span className="dice_size">{props.dice_size}</span>{' '}
-        <span className="damage_type">{props.damage_type}</span>
-        {' damage on hit.'}
+        <div className="damage_info">
+          <span className="dice_count">{props.dice_count}</span>
+          {'d'}
+          <span className="dice_size">{props.dice_size}</span>
+          {' + '}
+          <span className="damage_addition">{props.modifier}</span>
+          <span className="damage_calculation">{` (${props.modifier_name}) `}</span>
+          <span className="damage_type">{props.damage_type}</span>
+          {/* {' damage on hit.'} */}
+        </div>
       </div>
+      <div className="weapon_properties">{'heavy, two-handed'}</div>
     </div>
   );
 }
--- src/components/Offense/Weapons.js
+++ src/components/Offense/Weapons.js
@@ -4,6 +4,14 @@ import Weapon from './Weapon';
 import '../../styles/Weapons.css';
 
 export default function Weapons(props) {
+  let finesse_name = 'dexterity modifier';
+  let finesse_modifier = props.dexterity_modifier;
+
+  if (props.strength_modifier > props.dexterity_modifier) {
+    finesse_name = 'strength modifier';
+    finesse_modifier = props.strength_modifier;
+  }
+
   return (
     <div className="weapons">
       <div className="weapons_title">
@@ -15,6 +23,7 @@ export default function Weapons(props) {
         weapon_type="melee_weapon"
         modifier={props.strength_modifier}
         proficiency={props.proficiency}
+        modifier_name="strength modifier"
         dice_count={2}
         dice_size={6}
         damage_type="slashing"
@@ -23,6 +32,7 @@ export default function Weapons(props) {
         name="Longbow"
         weapon_type="range_weapon"
         modifier={props.dexterity_modifier}
+        modifier_name="strength modifier"
         proficiency={props.proficiency}
         dice_count={1}
         dice_size={6}
@@ -31,8 +41,9 @@ export default function Weapons(props) {
       <Weapon
         name="Rapier"
         weapon_type="finesse_weapon"
-        modifier={Math.max(props.dexterity_modifier, props.strength_modifier)}
+        modifier={finesse_modifier}
         proficiency={props.proficiency}
+        modifier_name={finesse_name}
         dice_count={1}
         dice_size={8}
         damage_type="piercing"
