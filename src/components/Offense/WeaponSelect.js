b6cca25ebe390dd8e1ebc29202103c67d30e0416
Riley Sherlock <riley.sherlock@mts.net>
2022-10-03 14:46:44 -0500

    fill in weapon select values

--- src/components/Offense/WeaponSelect.js
+++ src/components/Offense/WeaponSelect.js
@@ -4,10 +4,42 @@ import { Weapons } from '../../info/Weapons';
 export default function WeaponSelect(props) {
   return (
     <select name="weapon_select" id="weapon_select">
-      <optgroup label="Simple Melee Weapons"></optgroup>
-      <optgroup label="Simple Ranged Weapons"></optgroup>
-      <optgroup label="Martial Melee Weapons"></optgroup>
-      <optgroup label="Martial Ranged Weapons"></optgroup>
+      <optgroup label="Simple Melee Weapons">
+        {Object.keys(Weapons.simple_melee).map((k) => {
+          return (
+            <option value={k} key={k}>
+              {Weapons.simple_melee[k].name}
+            </option>
+          );
+        })}
+      </optgroup>
+      <optgroup label="Simple Ranged Weapons">
+        {Object.keys(Weapons.simple_ranged).map((k) => {
+          return (
+            <option value={k} key={k}>
+              {Weapons.simple_ranged[k].name}
+            </option>
+          );
+        })}
+      </optgroup>
+      <optgroup label="Martial Melee Weapons">
+        {Object.keys(Weapons.martial_melee).map((k) => {
+          return (
+            <option value={k} key={k}>
+              {Weapons.martial_melee[k].name}
+            </option>
+          );
+        })}
+      </optgroup>
+      <optgroup label="Martial Ranged Weapons">
+        {Object.keys(Weapons.martial_range).map((k) => {
+          return (
+            <option value={k} key={k}>
+              {Weapons.martial_range[k].name}
+            </option>
+          );
+        })}
+      </optgroup>
     </select>
   );
 }
