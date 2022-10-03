9bebe62323407ad65efed1ac9de7a705f60ecd4b
Riley Sherlock <riley.sherlock@mts.net>
2022-10-03 17:05:57 -0500

    fix finesse weapons not being calculated properly

--- src/components/Offense/Weapon.js
+++ src/components/Offense/Weapon.js
@@ -2,25 +2,37 @@ import React from 'react';
 import useStatistic from '../../hooks/useStatistic';
 
 export default function Weapon(props) {
-  const [visual, setVisual] = useStatistic(props.weapon_type);
+  // const [meleeVisual, setMeleeVisual] = useStatistic('melee_weapon');
+  // const [rangeVisual, setRangeVisual] = useStatistic('range_weapon');
+  // const [finesseVisual, setFinesseVisual] = useStatistic('finesse_weapon');
 
-  function toggleVisual(e) {
-    e.preventDefault();
-    setVisual(visual === 'focused' ? 'none' : 'focused');
+  // function toggleVisual(e) {
+  //   e.preventDefault();
+  //   setVisual(visual === 'focused' ? 'none' : 'focused');
+  // }
+
+  let to_hit_modifier;
+  let damage;
+  let modifier;
+
+  function setModifier(source) {
+    if (source === 'dexterity') {
+      to_hit_modifier = props.dexterity_modifier;
+      damage = props.dexterity_modifier;
+      modifier = 'dexterity';
+    } else if (source === 'strength') {
+      to_hit_modifier = props.strength_modifier;
+      damage = props.strength_modifier;
+      modifier = 'strength';
+    }
   }
 
-  let to_hit_modifier = props.dexterity_modifier;
-  let damage = props.dexterity_modifier;
-  let modifier = 'dexterity';
-  if (
-    props.weapon.melee ||
-    (props.weapon.properties.includes('finesse') &&
-      props.strength_modifier > props.dexterity_modifier)
-  ) {
-    to_hit_modifier = props.strength_modifier;
-    damage = props.strength_modifier;
-    modifier = 'strength';
-  }
+  if (props.weapon.properties.includes('finesse')) {
+    if (props.dexterity_modifier > props.strength_modifier)
+      setModifier('dexterity');
+    else setModifier('strength');
+  } else if (props.weapon.melee) setModifier('strength');
+  else setModifier('dexterity');
 
   let to_hit = to_hit_modifier + props.proficiency;
 
