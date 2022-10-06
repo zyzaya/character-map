fdcb0f1d4913a93b90892cee59e0e5491baad538
Riley Sherlock <riley.sherlock@mts.net>
2022-10-06 13:43:31 -0500

    make clicking weapon properties open info popup

--- src/components/Offense/WeaponProperty.js
+++ src/components/Offense/WeaponProperty.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import '../../styles/WeaponProperty.css';
+import { setVisible, setValue } from '../../state/infoSlice';
+import { useDispatch } from 'react-redux';
 
 export default function WeaponProperty(props) {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    dispatch(setValue(props.name));
+    dispatch(setVisible(true));
+  }
+
   return (
     <button className="weapon_property">
-      <span>{`${props.name}`}</span>
+      <span onClick={handleClick}>{`${props.name}`}</span>
       <span>{`${props.last ? '' : ', '}`}</span>
     </button>
   );
