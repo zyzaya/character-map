35b9b059ead344b6d0a8719452681cff78e00f0c
Riley Sherlock <riley.sherlock@mts.net>
2022-10-17 14:22:14 -0500

    add textual explanation of legend

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -5,19 +5,25 @@ import RightArrow from '../right-arrow.png';
 export default function Legend(props) {
   return (
     <div className="legend">
-      <div className="sourced">
-        <div className="legend_text">
-          Values that affect the Current Selection
+      <div className="legend_explanation">
+        Click on a value to change the current selection, displaying what values
+        affect the selection and what values are affected by the selection.
+      </div>
+      <div className="legend_display">
+        <div className="legend_sourced sourced">
+          <div className="legend_text">
+            Values that affect the Current Selection
+          </div>
         </div>
-      </div>
-      <img className="legend_arrow" src={RightArrow} alt="RightArrow" />
-      <div className="focused">
-        <div className="legend_text">Current Selection</div>
-      </div>
-      <img className="legend_arrow" src={RightArrow} alt="RightArrow" />
-      <div className="affected">
-        <div className="legend_text">
-          Values that are affected by the Curent Selection
+        <img className="legend_arrow" src={RightArrow} alt="RightArrow" />
+        <div className="legend_focused focused">
+          <div className="legend_text">Current Selection</div>
+        </div>
+        <img className="legend_arrow" src={RightArrow} alt="RightArrow" />
+        <div className="legend_affected affected">
+          <div className="legend_text">
+            Values that are affected by the Curent Selection
+          </div>
         </div>
       </div>
     </div>
